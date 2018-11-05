c83334c3104e727aaf00f99afb9b2234e375267e
atleasticode <atleasticode@users.noreply.github.com>
2018-11-05 21:42:16 +0100

    Delete wish.js

--- wish.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Wish {
-    constructor(doneeName, wishName, wishExpense){
-        this.doneeName = doneeName
-        this.wishName = wishName
-        this.wishExpense = wishExpense
-        this.votes = 0
-    }
-}
